7f18bd23a4fdfcbfb00777aa5b509b587eb28bd6
Cesar Borges <cesar_silk321@hotmail.com>
2022-07-14 15:59:35 -0300

    novos icones

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faCloudSun, faCoffee, faGear, faMapLocation, faMapLocationDot, faWind} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tabs',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+
+  icons = {
+    coffe: faCoffee,
+    weather: faCloudSun,
+    winds: faWind,
+    map: faMapLocationDot,
+    gear: faGear
+  };
 
   constructor() {}
 
